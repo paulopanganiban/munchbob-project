6051bbec1ca2983aca5614877821feac7b1fa9f1
John Paulo Panganiban <olopangs@gmail.com>
2021-08-23 22:48:25 +0800

    asd

---
+++ data.js
@@ -0,0 +1,10 @@
+// Adobo Rice - pork
+// Bistek Tagalog - beef
+// Beef Salpicao - beef
+// Pork Binagoongan - pork
+// Sisig Rice - pork
+// Tinapa with Aligue Rice - seafood/fish
+
+// Chicken Teriyaki - chicken
+// Kimchi Fried Rice - pork
+// Orange Chicken - chicken
